fdd52800b0e039ecba0f2d955ed197fff4d848f5
Antoine Baudrand <7fELF@users.noreply.github.com>
2024-07-12 11:17:51 +0200

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,6 +4,6 @@ class etcd::install {
   if $::etcd::manage_package {
     $ensure = $::etcd::ensure ? { 'held' => present, default => $::etcd::ensure }
     $mark = $::etcd::ensure ? { 'held' => hold, default => none }
-    package { $::etcd::package_name: ensure => $::etcd::ensure, }
+    package { $::etcd::package_name: ensure => $ensure, mark => $mark }
   }
 }
